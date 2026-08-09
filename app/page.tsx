90b8b5a0f079e8ddc8b10756ea414a89ce795897
AhmadSyaukiM <forbuisness694@gmail.com>
2026-08-09 19:07:50 +0700

    my first web with nextjs /love

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className="text-center text-2xl">Hallo teman teman !
+        <h1 className="text-5xl text-center font-bold">ini adalah aplikasi pertama   saya dengan next js</h1>
+          <p className="mt-4 text-gray-400">Sebagai Web Developer & UI/UX Designer</p>
+          </div>
+    </main>
+  );
+}
+
